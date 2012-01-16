3deed38464495ba7c0386ee3d1d9c2f6140ef9b7
Josef Šimánek <retro@ballgag.cz>
2012-01-16 22:29:35 +0100

    Comments

--- lib/get_out.rb
+++ lib/get_out.rb
@@ -3,18 +3,27 @@ require 'cinch'
 
 module GetOut
   def self.now!
+
+    # We are starting a new IRC bot.
     bot = Cinch::Bot.new do
       configure do |c|
+        # By default it will use irc.cservers.cz server
+        # with Buh nick and will join #cservers channel.
         c.server   = "irc.cservers.cz"
         c.nick     = "Buh"
         c.channels = ["#cservers"]
       end
 
+      # When some types exactly "Prosím ať NICK strašně vypadne"
+      # it will automaticaly respond into channel with message
+      # "NICK: Strašně, ale strašně, vypadni!"
+      # So that one lucky user will get notification.
       on :message, /^Prosím, ať (.+?) strašně vypadne :\(/ do |m, who|
         m.reply "#{who}: Strašně, ale strašně, vypadni!"
       end
     end
 
+    # Finally, we need to start this circus.
     bot.start
   end
 end
